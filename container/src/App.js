a94803fe1c7a317b155cc372d1515d76e0d38ca4
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-05-28 14:46:20 +0800

    testing pipeline #4

--- container/src/App.js
+++ container/src/App.js
@@ -3,6 +3,8 @@ import MarketingApp from "./components/marketingApp";
 export default () => {
   return (
     <div>
+      <h1>Updated Container</h1>
+      <hr />
       <MarketingApp />
     </div>
   );
