c0b2e6b29d88a8924e8df78d6fb01010dab4fc67
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-05-29 06:38:55 +0800

    finished feature

--- container/src/App.js
+++ container/src/App.js
@@ -3,7 +3,6 @@ import MarketingApp from "./components/marketingApp";
 export default () => {
   return (
     <div>
-      <h1>Updated Container</h1>
       <hr />
       <MarketingApp />
     </div>
