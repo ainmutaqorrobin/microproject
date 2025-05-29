493cd3a864755c0ed4b572aecd35e0b6cc6b3b26
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-05-29 13:52:00 +0800

    add prefix for container app

--- container/src/App.js
+++ container/src/App.js
@@ -1,14 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import Header from "./components/Header";
 import MarketingApp from "./components/marketingApp";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
