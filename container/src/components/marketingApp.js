edc9303f7815d8d180e8eeab975b087953657957
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-05-31 20:36:32 +0800

    implementing multi tier navigation between apps

--- container/src/components/marketingApp.js
+++ container/src/components/marketingApp.js
@@ -1,12 +1,22 @@
 import { renderMarketing } from "marketingTeam/MarketingApp";
 import { useEffect, useRef } from "react";
+import { useHistory } from "react-router-dom";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    renderMarketing(ref.current);
+    const { onParentNavigate } = renderMarketing(ref.current, {
+      onNavigate: ({ pathname: nextPath }) => {
+        const { pathname } = history.location;
+        if (pathname !== nextPath) history.push(nextPath);
+      },
+      initialPathname: history.location.pathname,
+    });
+
+    history.listen(onParentNavigate);
   }, []);
-  
+
   return <div ref={ref} />;
 };
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,15 +1,31 @@
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createMemoryHistory } from "history";
 
-const renderMarketing = (htmlElement) => {
-  ReactDOM.render(<App />, htmlElement);
+const renderMarketing = (htmlElement, { onNavigate, initialPathname }) => {
+  const history = createMemoryHistory();
+
+  if (onNavigate) history.listen(onNavigate);
+  if (initialPathname) {
+    const { pathname } = history.location;
+    if (pathname !== initialPathname) history.push(initialPathname);
+  }
+
+  ReactDOM.render(<App history={history} />, htmlElement);
+
+  return {
+    onParentNavigate({ pathname: nextPath }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPath) history.push(nextPath);
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const element = document.querySelector("#_local-marketing");
 
   if (element) {
-    renderMarketing(element);
+    renderMarketing(element, {});
   }
 }
 
