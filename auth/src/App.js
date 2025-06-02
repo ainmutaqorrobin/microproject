850f67096cd9b142568199b8e8c8933bcefa42a5
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-06-02 14:38:27 +0800

    added Auth app

---
+++ auth/src/App.js
@@ -0,0 +1,29 @@
+import { Switch, Route, Router } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+import SignIn from "./components/Signin";
+import SignUp from "./components/Signup";
+
+//to avoid css coalition bug, so renamed the prefix name with different tag
+const generateClassName = createGenerateClassName({
+  productionPrefix: "au",
+});
+
+function App({ history }) {
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+            <Route path="/auth/signin" component={SignIn} />
+            <Route path="/auth/signup" component={SignUp} />
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ auth/src/bootstrap.js
@@ -0,0 +1,35 @@
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createMemoryHistory, createBrowserHistory } from "history";
+
+const renderMarketing = (
+  htmlElement,
+  { onNavigate, initialPathname, defaultHistory }
+) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) history.listen(onNavigate);
+  if (initialPathname) {
+    const { pathname } = history.location;
+    if (pathname !== initialPathname) history.push(initialPathname);
+  }
+
+  ReactDOM.render(<App history={history} />, htmlElement);
+
+  return {
+    onParentNavigate({ pathname: nextPath }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPath) history.push(nextPath);
+    },
+  };
+};
+
+if (process.env.NODE_ENV === "development") {
+  const element = document.querySelector("#_local-auth");
+
+  if (element) {
+    renderMarketing(element, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+export { renderMarketing };
