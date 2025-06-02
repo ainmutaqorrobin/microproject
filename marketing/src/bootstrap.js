54d32c7568b2fa0c565e6490d0cd050496bc68c7
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-06-02 14:03:32 +0800

    add defaultHistory for marketing app (isolation purpose)

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,9 +1,12 @@
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createMemoryHistory } from "history";
+import { createMemoryHistory, createBrowserHistory } from "history";
 
-const renderMarketing = (htmlElement, { onNavigate, initialPathname }) => {
-  const history = createMemoryHistory();
+const renderMarketing = (
+  htmlElement,
+  { onNavigate, initialPathname, defaultHistory }
+) => {
+  const history = defaultHistory || createMemoryHistory();
 
   if (onNavigate) history.listen(onNavigate);
   if (initialPathname) {
@@ -25,7 +28,7 @@ if (process.env.NODE_ENV === "development") {
   const element = document.querySelector("#_local-marketing");
 
   if (element) {
-    renderMarketing(element, {});
+    renderMarketing(element, { defaultHistory: createBrowserHistory() });
   }
 }
 
